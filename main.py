151e3e159f378e61590a98c03bba1b79598ac24b
Yousef Amirghofran <yamirghofran@gmail.com>
2025-06-12 14:09:45 +0200

    feat: add messages as types.Content for multi-turn conversations

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ if len(sys.argv) < 2:
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
+user_prompt = sys.argv[1]
+messages = types.Content(role="user", parts=[types.Part(text=user_prompt)])
 
 client = genai.Client(api_key=api_key)
-res = client.models.generate_content(
-    model="gemini-2.0-flash-001",
-    contents=sys.argv[1],
-)
+res = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
 print(res.text)
 print("Prompt tokens:", res.usage_metadata.prompt_token_count)
 print("Response tokens:", res.usage_metadata.candidates_token_count)
